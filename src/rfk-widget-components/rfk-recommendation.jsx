38f7445981ced1452d30f4596fdb347d69cec8e6
cynthia <cricomendoza@four51.com>
2022-06-27 17:45:50 -0500

    add carousel for recommendation widget

--- src/rfk-widget-components/rfk-recommendation.jsx
+++ src/rfk-widget-components/rfk-recommendation.jsx
@@ -1,52 +1,65 @@
-import { trackPDPViewEvent } from "@sitecore-discover/react";
-import { Container, Grid, Paper } from "@material-ui/core";
+import { Container } from "@material-ui/core";
+import {
+  defaultRequests,
+  PageController,
+  trackPDPViewEvent,
+  WidgetDataType,
+} from "@sitecore-discover/react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import { useNavigate } from "react-router-dom";
+import ProductCard from "../components/shared/product-card";
 
 const RfkRecommendation = ({
-	loading,
-	products = [],
-	onProductClick,
-	// can i get more context on what/why this is needed? for analytics?
-	onNavigationNext,
-	onNavigationPrev
+  loading,
+  products = [],
+  onProductClick,
+  onNavigationNext,
+  onNavigationPrev,
+  title,
 }) => {
-	const styleObject = {
-		width : 200, 
-		align : "center"
-	}
-	const paperStyles = {
-		height: 400,
-		width: 300
-	}
-	const onPDPEventBtnClick = (sku) => {
-		trackPDPViewEvent(sku)
-		onProductClick(sku)
-	}
-	return (
-    <Container>
+  const navigate = useNavigate();
+  const onPDPEventBtnClick = (sku) => {
+    trackPDPViewEvent(sku);
+    onProductClick(sku);
+    navigate(`/products/detail/${sku}`);
+  };
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 4,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+  return (
+    <Container style={{height: '100%'}}>
       {!loading && products.length ? (
         <div>
-			<h1> Products For You</h1>
-			<Grid container spacing={2}>
-				<Grid item xs={12}>
-					<Grid container justifyContent="center" spacing={3}>
-						{products.map(product => (
-							<Grid key={product.sku} item>
-								<Paper style={paperStyles}>
-									<h5 className="my-3 mx-2">{product.name}</h5>
-									<div className="mx-auto" style={styleObject}>
-										<img width={200} src={product.image_url} />
-										<br />
-										<b>Price: {product.price}</b>
-										<br />
-										<button onClick={() => onPDPEventBtnClick(product.sku)}> create pdp view event</button>
-										<button onClick={() => onNavigationNext({index: 3})}> what does this do?</button>
-									</div>
-								</Paper>
-							</Grid>
-						))}
-					</Grid>
-				</Grid>
-			</Grid>			
+          <h1> {title}</h1>
+          <Carousel responsive={responsive}>
+            {products.map((product) => (
+				<div style={{display: 'flex !important'}}>
+					<ProductCard
+					  key={product.sku}
+					  {...product}
+					  onClick={() => onPDPEventBtnClick(product.sku)}
+					  isPreviewSearch={false}
+					/>
+				</div>
+            ))}
+          </Carousel>
         </div>
       ) : (
         <div> Loading ... </div>
